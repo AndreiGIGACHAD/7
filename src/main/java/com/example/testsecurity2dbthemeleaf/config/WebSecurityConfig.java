175248675f15ddaf9a163850fff0f6f100a11fa9
bratu <bratunbratka@gmail.com>
2023-09-27 12:22:41 +0500

    f

---
+++ src/main/java/com/example/testsecurity2dbthemeleaf/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.testsecurity2dbthemeleaf.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public static PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers
+            .antMatchers
+            .and()
+            .formLogin(
+                    form -> form
+                            .loginPage("/login")
+                            .loginProcessingUrl("/login")
+                            .defaultSuccessUrl("/users")
+                            .permitAll()
+
+). Logout(
+        logout -> logout
+                .logoutRequestMatcher (new AntPathRequestMatcher("/Logout"))
+                .permitAll()
+                );
+return http.build();
+}
+}
